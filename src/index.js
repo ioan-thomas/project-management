85a5a105b13df1397b4b8ea99e66792daaf07b97
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-13 16:45:46 +0100

    Shared context with all child components of the App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { AuthContextProvider } from './context/AuthContext';
 import App from './App';
+// styles
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
