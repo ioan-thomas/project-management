7df86204e8c42d4e6f80c8c7c6fdefa7f4e859ad
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-13 18:20:33 +0100

    created routes for the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+// pages
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
+import Create from './pages/create/Create'
+
+// styles
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
 
-    </div>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>  
+
+          <Route path="/login">
+            <Login />
+          </Route>  
+
+          <Route path="/projects/:id">
+            <Project />
+          </Route>  
+
+          <Route path="/signup">
+            <Signup />
+          </Route>  
+
+          <Route path="/create">
+            <Create />
+          </Route>  
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
