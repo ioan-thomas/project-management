78a09d4c74b0b3e7f1354fda353d4dba1630039c
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-13 18:59:22 +0100

    added a navbar component with elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
 import './Navbar.css'
-import Temple from '../assets/temple.svg'
+import Temple from '../assets/temple.svg' 
 
-import { Link } from 'react-router-dom'
-import React from 'react'
+import { Link } from 'react-router-dom' 
+import React from 'react' 
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
+    <div className='navbar'> 
         <ul>
             <li className='logo'>
-                <img src={Temple} alt="dojo icon"/>
-                <span>The dojo</span>
+                <img src={Temple} alt="dojo icon"/> 
+                <span>The dojo</span> 
             </li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <li><button className='btn'>Logout</button></li>
+            <li><Link to="/login">Login</Link></li> 
+            <li><Link to="/signup">Signup</Link></li> 
+            <li><button className='btn'>Logout</button></li> 
         </ul>
     </div>
   )
