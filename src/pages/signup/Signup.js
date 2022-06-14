055d22e086f0ebd407020e81a2d7563afb7ca911
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-14 13:21:43 +0100

    handled file input changes

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -8,9 +8,33 @@ export default function Signup() {
   const [password, setPassword] = useState('');
   const [displayName, setDisplayName] = useState('');
   const [thumbnail, setThumbnail] = useState(null);
+  const [thumbnailError, setThumbnailError] = useState(null)
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(email, password, displayName, thumbnail)
+  }
+
+  const handleFileChange = (e) => {
+    setThumbnail(null)
+    let selected = e.target.files[0];
+
+    if(!selected){
+      setThumbnailError('Please select a file')
+      return
+    }
+    if (!selected.type.includes('image')){
+      setThumbnailError('Selected file myst be an image')
+      return
+    }
+    if (selected.size > 100000){
+      setThumbnailError('Image file size must be less than 100kb')
+      return
+    }
+
+    setThumbnailError(null);
+    setThumbnail(selected);
+    console.log('thumbnail updated')
   }
 
   return (
@@ -47,8 +71,10 @@ export default function Signup() {
         <input 
         required 
         type="file"  
+        onChange={handleFileChange}
         />
       </label>
+      {thumbnailError && <div className='error'>{thumbnailError}</div>}
       <button className='btn'>Sign up</button>
     </form>
   )
