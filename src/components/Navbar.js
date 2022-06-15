b7d5c09580560635523aee33db9290a96e5f4ac0
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-15 14:21:53 +0100

    conditionally showing navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,11 @@ import Temple from '../assets/temple.svg'
 
 import { Link } from 'react-router-dom' 
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Navbar() {
   const {logout, isPending, error} = useLogout()
+  const {user} = useAuthContext()
 
   return (
     <div className='navbar'> 
@@ -15,9 +17,10 @@ export default function Navbar() {
                 <span>The dojo</span> 
             </li>
             {error && <li><p>{error}</p></li>}
-            <li><Link to="/login">Login</Link></li> 
-            <li><Link to="/signup">Signup</Link></li> 
-            {!isPending && <li><button className='btn' onClick={logout}>Logout</button></li> }
+            {!user && <li><Link to="/login">Login</Link></li> }
+            {!user && <li><Link to="/signup">Signup</Link></li> }
+
+            {(!isPending && user) && <li><button className='btn' onClick={logout}>Logout</button></li> }
             {isPending && <li><button className='btn' disabled>Logging out...</button></li> }
         </ul>
     </div>
