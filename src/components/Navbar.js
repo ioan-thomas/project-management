3d7e6837882c4a9c5f2c58a779847ea8ae6319c0
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-15 13:14:52 +0100

    logging users out when logout button clicked

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import './Navbar.css'
 import Temple from '../assets/temple.svg' 
 
 import { Link } from 'react-router-dom' 
-import React from 'react' 
+import { useLogout } from '../hooks/useLogout'
 
 export default function Navbar() {
+  const {logout, isPending} = useLogout()
+
   return (
     <div className='navbar'> 
         <ul>
@@ -14,7 +16,8 @@ export default function Navbar() {
             </li>
             <li><Link to="/login">Login</Link></li> 
             <li><Link to="/signup">Signup</Link></li> 
-            <li><button className='btn'>Logout</button></li> 
+            {!isPending && <li><button className='btn' onClick={logout}>Logout</button></li> }
+            {isPending && <li><button className='btn' disabled>Logging out...</button></li> }
         </ul>
     </div>
   )
