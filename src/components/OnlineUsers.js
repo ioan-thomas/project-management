9f9ac52fffe32304e0c915096ea73eb179c77cac
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-15 19:02:28 +0100

    output user online status symbol

--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -12,6 +12,7 @@ export function OnlineUsers() {
         {error && <div className='error'>{error}</div>}
         {documents?.map(user => (
             <div key={user.id} className='user-list-item'>
+                {user.online && <span className='online-user'></span> }
                 <span>{user.displayName}</span>
                 <Avatar src={user.photoURL}/>
             </div>
