03b31b7186619959dbed56ed9e4f4eabfa8a8ddd
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-16 14:05:53 +0100

    created a grid template for each project

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,15 +1,28 @@
-
+import { Link } from 'react-router-dom'
+import {Avatar} from './Avatar'
 
 // styles
 import './ProjectList.css'
 
 export default function ProjectList({projects}) {
   return (
-    <>
+    <div className='project-list'>
         {projects.length === 0 && <p>No projects here yet!</p>}
-        {projects.map(doc => (
-        <p key={doc.id}>{doc.name}</p>
+        {projects.map(project => (
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <ul>
+              {project.assignedUserList.map(user => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL}/>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
         ))}
-    </>
+    </div>
   )
 }
