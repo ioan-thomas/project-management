f92dafac9238f187f4d5c0c00f18f526a8d79a30
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-16 14:32:14 +0100

    fetching data form specified collection by id

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,5 +1,26 @@
-export function useDocument() {
-  return (
-    <div>useDocument</div>
-  )
+import { projectFirestore } from "../firebase/config"
+import { useEffect, useState } from "react"
+
+export function useDocument(collection, id) {
+    const [document, setDocument] = useState(null);
+    const [error, setError] = useState(null);
+
+    // realtime data
+
+    useEffect(() => {
+        const ref = projectFirestore.collection(collection).doc(id)
+
+        const unsub = ref.onSnapshot(doc => {
+            setDocument({...doc.data(), id: doc.id})
+            setError(null)
+        }, err => {
+            console.log(err.message);
+            setError('failed to get document')
+        })
+
+        return () => unsub();
+
+    }, [collection, id])
+
+    return [document, error]
 }
