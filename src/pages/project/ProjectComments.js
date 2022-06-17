cf7957d0b54f8c953abaf812093063c256aa5f0b
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-17 09:13:40 +0100

    outputting the comment info retrieved from the useDocument hook snapshot

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -2,11 +2,12 @@ import { useState } from "react"
 import { timestamp } from "../../firebase/config"
 import {useAuthContext} from '../../hooks/useAuthContext'
 import { useFirestore } from "../../hooks/useFirestore"
+import {Avatar} from '../../components/Avatar'
 
 // styles
 import './Project.css'
 
-export default function ProjectComments({project}) {
+export default function ProjectComments({project:{id, comments}}) {
     
     const {updateDocument, response:{error, isPending}} = useFirestore('projects')
     const [newComment, setNewComment] = useState('')
@@ -24,8 +25,8 @@ export default function ProjectComments({project}) {
             id: Math.random()
         }
 
-        await updateDocument(project.id, {
-            comments: [...project.comments, commentToAdd]
+        await updateDocument(id, {
+            comments: [...comments, commentToAdd]
         })
 
         if(!error) {
@@ -38,6 +39,22 @@ export default function ProjectComments({project}) {
   return (
     <div className="project-comments">
         <h4>Project Comments</h4>
+        <ul>
+            {comments.length > 0 && comments.map(comment => (
+                <li key={comment.id}>
+                    <div className="comment-author">
+                        <Avatar src={comment.photoURL}/>
+                        <p>{comment.displayName}</p>
+                    </div>
+                    <div className="comment-date">
+                        <p>date here</p>
+                    </div>
+                    <div className="comment-content">
+                        <p>{comment.content}</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
         <form className="add-comment" onSubmit={handleSubmit}>
             <label>
                 <span>Add a new comment:</span>
