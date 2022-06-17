96d3098c1f494c461aa93cb974a6cc0dbe25cad8
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-17 09:48:39 +0100

    allowed user who created project to delete the project - button is only visible to them too

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,27 +1,44 @@
 import {Avatar} from '../../components/Avatar'
+import {useFirestore} from '../../hooks/useFirestore'
+import {useAuthContext} from '../../hooks/useAuthContext'
+import { useHistory } from 'react-router-dom'
 
 import './Project.css'
 
-export default function ProjectSummary({project}) {
+export default function ProjectSummary({project:{name, details, dueDate, assignedUserList,createdBy:{id:createdById, displayName:createdDisplayName}, id:projId}}) {
+
+    const {deleteDocument} = useFirestore('projects')
+    const {user:{uid}} = useAuthContext()
+    const {push} = useHistory()
+
+    const handleClick = (e) => {
+        deleteDocument(projId)
+        push('/')
+    }
+
   return (
     <div>
         <div className="project-summary">
-            <h2 className="page-title">{project.name}</h2>
+            <h2 className="page-title">{name}</h2>
+            <p>By {createdDisplayName}</p>
             <p className='due-date'>
-                Project due by {project.dueDate.toDate().toDateString()}
+                Project due by {dueDate.toDate().toDateString()}
             </p>
             <p className="details">
-                {project.details}
+                {details}
             </p>
             <h4>Project is assigned to:</h4>
             <div className="assigned-users">
-                {project.assignedUserList.map(user => (
+                {assignedUserList.map(user => (
                     <div key={user.id}>
                         <Avatar src={user.photoURL}/>
                     </div>
                 ))}
             </div>
         </div>
+            {uid === createdById && (
+            <button className="btn" onClick={handleClick}>Mark as complete</button>
+            )}
     </div>
   )
 }
